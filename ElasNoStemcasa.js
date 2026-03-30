e35d2c8c608d4903f8b033e5e5df795a384d2173
jennynialves <jennynialves@gmail.com>
2026-03-30 19:57:31 -0300

    INICIO DO PROJETO

---
+++ ElasNoStemcasa.js
@@ -0,0 +1,19 @@
+
+var display = false
+document.getElementById("inicio").style.display = "block";
+document.getElementById("conteudo").style.display = "none";
+
+function abrir() {
+    display = !display
+    console.log(display)
+
+    if (display == true) {
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("conteudo").style.display = "block";
+    } else {
+        document.getElementById("inicio").style.display = "block";
+        document.getElementById("conteudo").style.display = "none";
+    }
+
+
+}
